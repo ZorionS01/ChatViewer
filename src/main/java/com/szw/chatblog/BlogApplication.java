8ff22da325acb1c301da2805b260a36465047bc7
xiaosong <2370760064@qq.com>
2023-10-13 11:14:40 +0800

    druid超过空闲等待,关闭空闲连接 -Ddruid.mysql.usePingMethod=false

---
+++ src/main/java/com/szw/chatblog/BlogApplication.java
@@ -0,0 +1,29 @@
+package com.szw.chatblog;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author ChatViewer
+ */
+@Slf4j
+@ServletComponentScan
+@EnableKafka
+@EnableTransactionManagement
+@SpringBootApplication
+public class BlogApplication {
+
+    static {
+        System.setProperty("druid.mysql.usePingMethod","false");
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
